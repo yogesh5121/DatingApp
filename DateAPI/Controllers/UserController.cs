c312d5b3ff5c9bcfd52098f961ba6f2d9416e348
YS3 <yogesh.s@capgemini.com>
2023-12-06 17:31:43 +0530

    Section1

---
+++ DateAPI/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DateAPI;
+[ApiController]
+[Route("api/[Controller]")]
+public class UserController: ControllerBase
+{
+      private readonly DataContext _context;
+    public UserController(DataContext context)
+    {
+        _context = context;
+    }
+    
+    [HttpGet]//get the all the data from database
+    public async Task<ActionResult<IEnumerable<AppUser>>>GetUser()
+    {
+        var res = await _context.Users.ToListAsync();
+        return Ok(res);
+    }
+    [HttpGet("{id}")]
+   
+    public async Task<ActionResult<AppUser>> GetById(int id)
+    {
+        var res = await _context.Users.FindAsync(id);
+        // Possible null reference argument.
+#pragma warning disable CS8604 // Possible null reference argument.
+        return res; // Possible null reference argument.
+#pragma warning restore CS8604 // Possible null reference argument.
+
+    }
+
+}
---
+++ DateAPI/Entities/AppUser.cs
@@ -0,0 +1,6 @@
+public class AppUser
+{
+    public int id { get; set; }
+    public required string UserName { get; set; }
+    
+}
